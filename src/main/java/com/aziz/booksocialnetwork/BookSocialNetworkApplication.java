a273d7a5c76e6d921597c7e1bdbe4e1eb5f4f637
aziztheiri <alatheiri123@gmail.com>
2024-12-24 18:40:37 +0100

    register and email

--- src/main/java/com/aziz/booksocialnetwork/BookSocialNetworkApplication.java
+++ src/main/java/com/aziz/booksocialnetwork/BookSocialNetworkApplication.java
@@ -2,12 +2,16 @@ package com.aziz.booksocialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class BookSocialNetworkApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookSocialNetworkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookSocialNetworkApplication.class, args);
+    }
 
 }
